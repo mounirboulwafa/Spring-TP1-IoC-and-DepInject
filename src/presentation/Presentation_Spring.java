ef44ac6a24ea2141d273d91c076aeb5be9f3ef37
Mounit BOULWAFA <mounirboulwafa@gmail.com>
2019-02-10 13:51:34 +0100

    DI using Spring IoC

--- src/presentation/Presentation_Spring.java
+++ src/presentation/Presentation_Spring.java
@@ -1,9 +1,20 @@
 package presentation;
 
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Presentation_Spring {
 
     public static void main(String[] args) {
+        /* Read the config.xlm file */
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+
+        IMetier metier = (IMetier) springContext.getBean("metier");
+
         System.out.println("**** Dependencies Injections (DI) ****");
+        System.out.println("Spring IoC Injection w xml file : ");
+        System.out.println(metier.calcule());
 
     }
 
